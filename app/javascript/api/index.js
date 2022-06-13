1f32eb7c89ae3378939cd091a391cb3e491cdbec
yadira <yadiraco304@gmail.com>
2022-06-13 11:44:16 -0500

    fix in heroku

--- app/javascript/api/index.js
+++ app/javascript/api/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const getUser = async () => {
-  const request = await axios.get("http://localhost:3000/profile");
+  const request = await axios.get("https://instagramruby.herokuapp.com/profile");
   const data = await request.data;
   return data;
 };
 
 const searchResults = async ({ search_id, text }) => {
   const request = await axios.post(
-    `http://localhost:3000/search/${search_id}`,
+    `https://instagramruby.herokuapp.com/search/${search_id}`,
     { text }
   );
   const data = await request.data;
@@ -16,13 +16,13 @@ const searchResults = async ({ search_id, text }) => {
 };
 
 const searchData = async ({ search_id }) => {
-  const request = await axios.get(`http://localhost:3000/search/${search_id}`);
+  const request = await axios.get(`https://instagramruby.herokuapp.com/search/${search_id}`);
   const data = await request.data;
   return data;
 };
 
 const postCreate = async ({ newData }) => {
-  const request = await axios.post("http://localhost:3000/post/create", {
+  const request = await axios.post("https://instagramruby.herokuapp.com/post/create", {
     ...newData,
   });
   const data = await request.data;
@@ -30,14 +30,14 @@ const postCreate = async ({ newData }) => {
 };
 
 const posts = async () => {
-  const request = await axios.get("http://localhost:3000/posts");
+  const request = await axios.get("https://instagramruby.herokuapp.com/posts");
   const data = await request.data;
   return data;
 };
 
 const commentCreate = async ({ postId, content }) => {
   const request = await axios.post(
-    `http://localhost:3000/comment/create/${postId}`,
+    `https://instagramruby.herokuapp.com/comment/create/${postId}`,
     { content }
   );
   const data = await request.data;
@@ -45,25 +45,25 @@ const commentCreate = async ({ postId, content }) => {
 };
 
 const userLogout = async () => {
-  const request = await axios.delete("http://localhost:3000/users/logout");
+  const request = await axios.delete("https://instagramruby.herokuapp.com/users/logout");
   const data = await request.data;
   return data;
 };
 
 const likeCreate = async ({ postId }) => {
-  const request = await axios.post(`http://localhost:3000/like/${postId}`);
+  const request = await axios.post(`https://instagramruby.herokuapp.com/like/${postId}`);
   const data = await request.data;
   return data;
 };
 
 const likeDelete = async ({ postId }) => {
-  const request = await axios.delete(`http://localhost:3000/like/${postId}`);
+  const request = await axios.delete(`https://instagramruby.herokuapp.com/like/${postId}`);
   const data = await request.data;
   return data;
 };
 
 const createHistory = async ({ url_image, url_video }) => {
-  const request = await axios.post(`http://localhost:3000/history`, {
+  const request = await axios.post(`https://instagramruby.herokuapp.com/history`, {
     history: {
       url_image,
       url_video,
@@ -74,7 +74,7 @@ const createHistory = async ({ url_image, url_video }) => {
 };
 
 const listHistories = async () => {
-  const request = await axios.get(`http://localhost:3000/histories`);
+  const request = await axios.get(`https://instagramruby.herokuapp.com/histories`);
   const data = await request.data;
   return data;
 };
