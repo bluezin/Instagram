473c1452cea154a9a62cd8fa33d7ecdee5f8570e
yadira <yadiraco304@gmail.com>
2026-09-19 11:30:52 -0500

    deploy 1

--- app/javascript/api/index.js
+++ app/javascript/api/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const getUser = async () => {
-  const request = await axios.get("https://instagramruby.herokuapp.com/profile");
+  const request = await axios.get("/profile");
   const data = await request.data;
   return data;
 };
 
 const searchResults = async ({ search_id, text }) => {
   const request = await axios.post(
-    `https://instagramruby.herokuapp.com/search/${search_id}`,
+    `/search/${search_id}`,
     { text }
   );
   const data = await request.data;
@@ -16,13 +16,13 @@ const searchResults = async ({ search_id, text }) => {
 };
 
 const searchData = async ({ search_id }) => {
-  const request = await axios.get(`https://instagramruby.herokuapp.com/search/${search_id}`);
+  const request = await axios.get(`/search/${search_id}`);
   const data = await request.data;
   return data;
 };
 
 const postCreate = async ({ newData }) => {
-  const request = await axios.post("https://instagramruby.herokuapp.com/post/create", {
+  const request = await axios.post("/post/create", {
     ...newData,
   });
   const data = await request.data;
@@ -30,14 +30,14 @@ const postCreate = async ({ newData }) => {
 };
 
 const posts = async () => {
-  const request = await axios.get("https://instagramruby.herokuapp.com/posts");
+  const request = await axios.get("/posts");
   const data = await request.data;
   return data;
 };
 
 const commentCreate = async ({ postId, content }) => {
   const request = await axios.post(
-    `https://instagramruby.herokuapp.com/comment/create/${postId}`,
+    `/comment/create/${postId}`,
     { content }
   );
   const data = await request.data;
@@ -45,25 +45,25 @@ const commentCreate = async ({ postId, content }) => {
 };
 
 const userLogout = async () => {
-  const request = await axios.delete("https://instagramruby.herokuapp.com/users/logout");
+  const request = await axios.delete("/users/logout");
   const data = await request.data;
   return data;
 };
 
 const likeCreate = async ({ postId }) => {
-  const request = await axios.post(`https://instagramruby.herokuapp.com/like/${postId}`);
+  const request = await axios.post(`/like/${postId}`);
   const data = await request.data;
   return data;
 };
 
 const likeDelete = async ({ postId }) => {
-  const request = await axios.delete(`https://instagramruby.herokuapp.com/like/${postId}`);
+  const request = await axios.delete(`/like/${postId}`);
   const data = await request.data;
   return data;
 };
 
 const createHistory = async ({ url_image, url_video }) => {
-  const request = await axios.post(`https://instagramruby.herokuapp.com/history`, {
+  const request = await axios.post(`/history`, {
     history: {
       url_image,
       url_video,
@@ -74,7 +74,7 @@ const createHistory = async ({ url_image, url_video }) => {
 };
 
 const listHistories = async () => {
-  const request = await axios.get(`https://instagramruby.herokuapp.com/histories`);
+  const request = await axios.get(`/histories`);
   const data = await request.data;
   return data;
 };
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "up", to: ->(_env) { [200, { "Content-Type" => "text/plain" }, ["OK"]] }
+
   get "/", to: "home#index"
 
   # login
